7976ea20877a29a98a98c9a6b858c7ff09779342
“yusufk06” <yusuf-9207@outlook.com>
2023-01-17 12:22:58 +0300

    17.ocak

---
+++ src/day36_genelTekrar/day01_RecursiveMethods.java
@@ -0,0 +1,35 @@
+package day36_genelTekrar;
+
+import java.util.Scanner;
+
+public class day01_RecursiveMethods {
+    //kullanicidan pozitif bir tamsayi alip
+    //o sayidan sifira kadar olan pozitif tamsayilari toplayin
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Pozitif bir tamsayi girin");
+        int sayi= scan.nextInt();
+
+        toplaForLoop(sayi);
+        System.out.println(toplaRecursive(sayi));
+
+
+    }
+
+    private static int toplaRecursive(int sayi) {
+        if (sayi>0){
+            return (sayi+toplaRecursive(sayi-1));
+        }else {
+            return sayi;
+        }
+    }
+
+    private static void toplaForLoop(int sayi) {
+        int toplam=0;
+        for (int i = 0; i <=sayi ; i++) {
+            toplam+=i;
+        }
+        System.out.println("Toplam for loop ile :"+ toplam);
+    }
+
+}
